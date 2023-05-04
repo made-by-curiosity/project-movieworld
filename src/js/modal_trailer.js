83fea9733c84f84d445df290b6b53fea6649f339
braunalex74 <braunalex74@googlemail.com>
2023-05-04 23:18:51 +0300

    add_js

---
+++ src/js/modal_trailer.js
@@ -0,0 +1,3 @@
+const showTrailerBtn = document.querySelector('.show-trailer-btn');
+const movieId = showTrailerBtn.CDATA_SECTION_NODE.movieId;
+const modal = document.querySelector('trailer-modal');
