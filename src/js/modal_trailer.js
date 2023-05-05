294fbadb16f098c7564df2ca6a418078b80ec469
braunalex74 <braunalex74@googlemail.com>
2023-05-05 21:48:54 +0300

    add_js

--- src/js/modal_trailer.js
+++ src/js/modal_trailer.js
@@ -1,3 +1,55 @@
 const showTrailerBtn = document.querySelector('.show-trailer-btn');
-const movieId = showTrailerBtn.CDATA_SECTION_NODE.movieId;
-const modal = document.querySelector('trailer-modal');
+const movieId = showTrailerBtn.dataset.movieId;
+const modal = document.querySelector('.trailer-modal');
+const trailerElement = modal.querySelector('.trailer');
+const errorMessage = modal.querySelector('.no-video-msg');
+
+function showTrailerModal(trailerUrl) {
+  trailerElement.setAttribute('src', trailerUrl);
+  modal.classList.add('show');
+}
+
+function hideTrailerModal() {
+  modal.classList.remove('show');
+  trailerElement.setAttribute('src', '');
+  errorMessage.style.display = 'none';
+}
+
+function showErrorModal() {
+  errorMessage.style.display = 'block';
+}
+
+showTrailerBtn.addEventListener('click', () => {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=YOUR_API_KEY&language=en-US`
+  )
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request error');
+    })
+    .then(movieData => {
+      const videos = movieData.results;
+      if (videos.length > 0) {
+        const videoKey = videos[0].key;
+        const trailerUrl = `https://www.youtube.com/embed/${videoKey}`;
+        showTrailerModal(trailerUrl);
+      } else {
+        showErrorModal();
+      }
+    })
+    .catch(error => {
+      console.error('Request video error', error);
+    });
+});
+
+modal.querySelector('.close-btn').addEventListener('click', function () {
+  hideTrailerModal();
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === modal) {
+    hideTrailerModal();
+  }
+});
