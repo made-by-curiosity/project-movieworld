fae20bf1ad3bc41e158bbdb65a7c2888f3dc97ba
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-05-07 00:06:52 +0300

    add fail4

--- src/js/weeklyTrends.js
+++ src/js/weeklyTrends.js
@@ -1,26 +1,26 @@
-const axios = require('axios').default;
-const API_KEY = 'f1b5155c1184f9f972000fc60d38fc3a';
+// const axios = require('axios').default;
+// const API_KEY = 'f1b5155c1184f9f972000fc60d38fc3a';
 
-export const getWeeklyTrends = async function getWeeklyTrends() {
-  const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
-  const params = {
-    api_key: API_KEY,
-  };
+// export const getWeeklyTrends = async function getWeeklyTrends() {
+//   const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
+//   const params = {
+//     api_key: API_KEY,
+//   };
 
-  const response = await axios.get(BASE_URL, { params });
+//   const response = await axios.get(BASE_URL, { params });
 
-  const weeklyTrends = await response.data;
-  console.log('weeklyTrends', weeklyTrends);
+//   const weeklyTrends = await response.data;
+//   console.log('weeklyTrends', weeklyTrends);
 
-  return weeklyTrends;
-};
-
-
+//   return weeklyTrends;
+// };
 
 
 
 
-export default function markUpFilms(data) {
+
+
+export default function getWeeklyTrends(data) {
   if (!data) {
     return;
   }
@@ -41,12 +41,12 @@ export default function markUpFilms(data) {
         }
 
         return `  
-        <li class="item" id="movieId">
+        <li class="item" id="${movieId}">
             <div class="item__image-box">
                 <img src="${BASE_URL + posterPath}" alt="Poster of ${title ? title : ''}" />
                 <div class="item__image-containet">
                     <p class="item__text">${title ? title : 'Title unavailable'}</p>
-                    <p class="item__text-color">${ } | ${movieDate ? movieDate.slice(0, 4) : 'Date unavailable'}
+                    <p class="item__text-color">${movieGenres} | ${movieDate ? movieDate.slice(0, 4) : 'Date unavailable'}
                     </p>
                 </div>
                 {/* <div class="item__icon">
