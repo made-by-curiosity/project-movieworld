0a442ec2b80e18aa5e0972056b8f03f87530e016
braunalex74 <braunalex74@googlemail.com>
2023-05-07 23:19:24 +0300

    update-js

--- src/js/modalTrailer.js
+++ src/js/modalTrailer.js
@@ -1,130 +1,49 @@
-const showTrailerBtn = document.querySelector('.show-trailer-btn');
-const modal = document.querySelector('.trailer-modal');
-const trailerElement = modal.querySelector('.trailer-element');
+import { getFullMovieInfo } from './fetchmoviedata.js';
 
-// Отримання теми сайту (світла або темна)
-function getSiteTheme() {
-  const siteTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light';
-  return siteTheme;
-}
-
-// Функція для завантаження трейлера по ідентифікатору фільму
-function loadTrailerById(movieId) {
-  fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=f1b5155c1184f9f972000fc60d38fc3a&language=en-US`
-  )
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Request error');
-    })
-    .then(movieData => {
-      const videos = movieData.results;
-      if (videos.length > 0) {
-        const videoKey = videos[0].key;
-        const trailerUrl = `https://www.youtube.com/embed/${videoKey}`;
-        const imageUrl = 'path/to/trailer-image.png';
-        showTrailerModal(trailerUrl, imageUrl);
-      } else {
-        showErrorModal();
-      }
-    })
-    .catch(error => {
-      console.error('Request video error', error);
-    });
-}
-
-// Показ модального вікна з трейлером
-function showTrailerModal(trailerUrl, imageUrl) {
-  trailerElement.setAttribute('src', trailerUrl);
-  modal.style.display = 'block';
-
-  const siteTheme = getSiteTheme();
-  updateModalBackground(siteTheme);
-
-  const errorContainer = modal.querySelector('.error-container');
-  if (errorContainer) {
-    modal.removeChild(errorContainer);
-  }
-
-  const trailerImage = document.createElement('img');
-  trailerImage.setAttribute('src', imageUrl);
-  trailerElement.appendChild(trailerImage);
-
-  trailerElement.contentWindow.postMessage(
-    '{"event":"command","func":"' + 'playVideo' + '","args":""}',
-    '*'
-  );
-
-  // Блокування подальшого поширення події кліку до елементу трейлера
-  trailerElement.addEventListener('click', event => {
-    event.stopPropagation();
-  });
-}
-
-// Ховання модального вікна з трейлером
-function hideTrailerModal() {
-  modal.style.display = 'none';
-  trailerElement.setAttribute('src', '');
-}
-
-// Відображення повідомлення про помилку разом із зображенням
-function showErrorModal() {
-  // Implement your error modal here
-}
-
-// Обробник кліку на кнопці "Show trailer"
-showTrailerBtn.addEventListener('click', () => {
-  const movieId = showTrailerBtn.dataset.movieId;
+watchTrailerBtn.addEventListener('click', () => {
+  const movieId = watchTrailerBtn.dataset.movieId;
   loadTrailerById(movieId);
 });
 
-// Ініціалізація бібліотеки Lightbox
-function initializeLightbox() {
-  const lightbox = new SimpleLightbox('.show-trailer-btn', {
-    elements: '.show-trailer-btn',
-    history: false,
-    docClose: false,
-    nav: false,
-    captionsData: 'alt',
-    captionDelay: 250,
-    captions: true,
-    captionSelector: 'self',
-    captionType: 'attr',
-    captionPosition: 'bottom',
-    captionsDataAlt: 'data-title',
-    captionAttribute: 'alt',
-  });
+const modal = document.querySelector('.trailer-modal');
+const movieContainer = modal.querySelector('.movie-container');
+const trailerElement = modal.querySelector('.trailer-element');
+const noVideoMsg = modal.querySelector('.no-video-msg');
 
-  // Додавання обробника кліку на кнопці "Show trailer" у Lightbox
-  const lightboxItems = lightbox.elements;
-  lightboxItems.forEach(item => {
-    const showTrailerBtn = item;
-    showTrailerBtn.addEventListener('click', event => {
-      event.stopPropagation();
-      const movieId = showTrailerBtn.dataset.movieId;
-      loadTrailerById(movieId);
-    });
-  });
+async function loadTrailerById(movieId) {
+  try {
+    const movieInfo = await getFullMovieInfo(movieId);
+    const videos = movieInfo.videos.results;
+    if (videos.length > 0) {
+      const videoKey = videos[0].key;
+      const trailerUrl = `https://www.youtube.com/embed/${videoKey}`;
+      showTrailerModal(trailerUrl, movieInfo.poster_path);
+    } else {
+      showNoVideoMessage(movieInfo.poster_path);
+    }
+  } catch (error) {
+    console.error('Request video error', error);
+  }
 }
 
-// Ініціалізація бібліотеки Lightbox після завантаження сторінки
-window.addEventListener('DOMContentLoaded', () => {
-  initializeLightbox();
-});
+function showTrailerModal(trailerUrl, posterUrl) {
+  trailerElement.setAttribute('src', trailerUrl);
+  movieContainer.innerHTML = `<img src="${posterUrl}" alt="Movie Poster">`;
+  modal.classList.remove('hidden');
+}
 
-// Обробник кліку на кнопці закриття
-const closeBtn = modal.querySelector('.close-btn');
-closeBtn.addEventListener('click', () => {
-  hideTrailerModal();
-});
+function showNoVideoMessage(posterUrl) {
+  noVideoMsg.textContent = 'No trailer available.';
+  movieContainer.innerHTML = `<img src="${posterUrl}" alt="Movie Poster">`;
+  modal.classList.remove('hidden');
+}
 
-// Обробник кліку поза модальним вікном
-window.addEventListener('click', event => {
-  if (event.target === modal) {
-    hideTrailerModal();
-  }
-});
+function closeTrailerModal() {
+  trailerElement.setAttribute('src', '');
+  noVideoMsg.textContent = '';
+  movieContainer.innerHTML = '';
+  modal.classList.add('hidden');
+}
+
+const movieId = '278';
+loadTrailerById(movieId);
