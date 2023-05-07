4944c55435c836a31f8e0b58e5a2c5e5814fcc07
Darina33 <115955389+Darina33@users.noreply.github.com>
2023-05-07 21:53:12 +0300

    cover

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,4 +1,5 @@
 import { getDayTrends } from "./fetchmoviedata";
+import { checkAverange } from "./checkrateaverage.js";
 
 const section = document.querySelector(".hero")
 
@@ -16,7 +17,7 @@ async function fetchMovie() {
 async function getHeroMarkup() {
   try {
     const { results } = await getDayTrends()
-    return createMarkup(results[5])
+    return createMarkup(results[1])
   }
   catch (err) {
           onError(err);
@@ -26,9 +27,10 @@ async function getHeroMarkup() {
 function createMarkup({ title, overview, backdrop_path, vote_average }) {
   const imageUrl = `url(https://image.tmdb.org/t/p/original${backdrop_path})`;
   const gradient = "linear-gradient(87.8deg, #0E0E0E 15.61%, rgba(14, 14, 14, 0) 60.39%)";
+
   section.style.backgroundImage = `${gradient}, ${imageUrl}`;
 
-    return `<div class="container"><h1 class="hero__title">${title}</h1><p class="hero__rating">${vote_average}</p>
+    return `<div class="container"><h1 class="hero__title">${title}</h1><img src='${checkAverange(vote_average)}' class="hero__rating"/>
         <p class="hero__text">${overview}</p>
         <button class="btn btn-main hero__btn">Watch trailer</button></div>`
 }
@@ -39,7 +41,18 @@ function updateHero(markup) {
 }
 
 function onError(err) {
-    console.error(err)
+  section.classList.add("hero__cover")
+  section.insertAdjacentHTML("beforeend", markupCover)
+  console.error(err)
 }
+
+
+const markupCover = `<div class="container">
+        <h1 class="hero__title">Let’s Make Your Own Cinema</h1>
+        <p class="hero__text">Is a guide to creating a personalized movie theater experience. You'll need a projector,
+            screen, and speakers.</p>
+        <button class="btn btn-main hero__btn">Get Started</button>
+    </div>`
+
 
 
