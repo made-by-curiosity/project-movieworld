1cdb6d34586ce94e530c647e8f1b8cc02ca9004d
Viktor Biruk <vikttur.job@gmail.com>
2023-05-08 22:05:45 +0300

    adds styles to the modal menu branch

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,34 @@
+// (() => {
+//   const refs = {
+//     mobileMenu: document.getElementById('mobile-menu'),
+//     openMenuBtn: document.querySelector('.js-open-menu'),
+//   };
+
+//   const onClickBtn = () => {
+//     const isMenuOpen =
+//       refs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     refs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     refs.mobileMenu.classList.toggle('is-open');
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', onClickBtn);
+// })();
+
 (() => {
   const refs = {
     mobileMenu: document.getElementById('mobile-menu'),
-    openMenuBtn: document.querySelector('.js-open-menu'),
+		openMenuBtn: document.querySelector('.js-open-menu'),
+		closeMenuBtn: document.getElementById('mobile-menu'),
   };
 
-  const onClickBtn = () => {
-    const isMenuOpen =
-      refs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    refs.mobileMenu.classList.toggle('is-open');
-  };
+	const onClickBtnOpen = () => {
+		refs.mobileMenu.classList.remove('js-hidden');
+	};
 
-  refs.openMenuBtn.addEventListener('click', onClickBtn);
+	const onClickBtnClose = () => {
+		refs.mobileMenu.classList.add('js-hidden');
+	};
+
+	refs.openMenuBtn.addEventListener('click', onClickBtnOpen);
+	refs.closeMenuBtn.addEventListener('click', onClickBtnClose);
 })();
