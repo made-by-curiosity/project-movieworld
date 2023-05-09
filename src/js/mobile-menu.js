8a989c1e11b9bb4c8b034891dc4fc7af904aefd6
Viktor Biruk <vikttur.job@gmail.com>
2023-05-09 10:52:13 +0300

    modifying the modal menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,28 +1,14 @@
-// (() => {
-//   const refs = {
-//     mobileMenu: document.getElementById('mobile-menu'),
-//     openMenuBtn: document.querySelector('.js-open-menu'),
-//   };
-
-//   const onClickBtn = () => {
-//     const isMenuOpen =
-//       refs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     refs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//     refs.mobileMenu.classList.toggle('is-open');
-//   };
-
-//   refs.openMenuBtn.addEventListener('click', onClickBtn);
-// })();
-
 (() => {
   const refs = {
     mobileMenu: document.getElementById('mobile-menu'),
 		openMenuBtn: document.querySelector('.js-open-menu'),
 		closeMenuBtn: document.getElementById('mobile-menu'),
+		navigationList: document.querySelectorAll('.mobile-menu__link'),
   };
-
+	
 	const onClickBtnOpen = () => {
 		refs.mobileMenu.classList.remove('js-hidden');
+		definitionActivePage();
 	};
 
 	const onClickBtnClose = () => {
@@ -32,3 +18,18 @@
 	refs.openMenuBtn.addEventListener('click', onClickBtnOpen);
 	refs.closeMenuBtn.addEventListener('click', onClickBtnClose);
 })();
+
+function definitionActivePage () {
+	if (document.location.pathname.includes('/page-catalog')) {
+		document.querySelector('.mobile-menu__link--galery').classList.add('current');
+		return;
+	};
+
+	if (document.location.pathname.includes('/page-my-library')) {
+		document.querySelector('.mobile-menu__link--library').classList.add('current');
+		return;
+	};
+
+	console.log('houm');
+	document.querySelector('.mobile-menu__link--home').classList.add('current');
+};
