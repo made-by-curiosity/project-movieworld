daa2c9e079d385a7b04af7cff4949a8ad29a6e0d
Viktor Biruk <vikttur.job@gmail.com>
2023-05-09 11:55:01 +0300

    disables scrolling when the menu is open

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -9,9 +9,11 @@
 	const onClickBtnOpen = () => {
 		refs.mobileMenu.classList.remove('js-hidden');
 		definitionActivePage();
+		document.body.classList.add('js-no-scroll');
 	};
 
 	const onClickBtnClose = () => {
+		document.body.classList.remove('js-no-scroll');
 		refs.mobileMenu.classList.add('js-hidden');
 	};
 
