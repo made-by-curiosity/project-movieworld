4144d7930e0275585c178105e32eacfa7b74a274
braunalex74 <braunalex74@googlemail.com>
2023-05-10 00:36:15 +0300

    beendet

--- src/js/modalTrailer.js
+++ src/js/modalTrailer.js
@@ -1,80 +1,62 @@
 import { getMovieTrailer } from './fetchmoviedata';
 
-document.querySelector('.hero').addEventListener('click', event => {
-  const watchTrailerBtn = event.target.closest('.hero__btn');
-  if (watchTrailerBtn) {
-    const movieId = watchTrailerBtn.getAttribute('data-id');
-    loadTrailerById(movieId);
-  }
-});
-
-const modal = document.querySelector('.trailer-modal');
-const movieContainer = modal.querySelector('.movie-container');
-const trailerElement = modal.querySelector('.trailer-element');
-const noVideoMsg = modal.querySelector('.no-video-msg');
-
-async function loadTrailerById(movieId) {
-  try {
-    const movieTrailer = await getMovieTrailer(movieId);
-    const videos = movieTrailer.results;
-    if (videos.length > 0) {
-      const videoKey = videos[0].key;
-      const trailerUrl = `https://www.youtube.com/embed/${videoKey}?autoplay=1`;
-      showTrailerModal(trailerUrl, movieTrailer.poster_path);
-    } else {
-      showNoVideoMessage(movieTrailer.poster_path);
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.hero').addEventListener('click', event => {
+    const watchTrailerBtn = event.target.closest('.hero__btn');
+    if (watchTrailerBtn) {
+      const movieId = watchTrailerBtn.getAttribute('data-id');
+      loadTrailerById(movieId);
     }
-  } catch (error) {
-    console.error('Request video error', error);
-  }
+  });
 
-  const closeButton = document.querySelector('.close-btn__svg');
-  closeButton.style.display = 'block';
-}
-
-function showTrailerModal(trailerUrl, imageUrl) {
-  trailerElement.setAttribute('src', trailerUrl);
-  trailerElement.style.width = '100%';
-  trailerElement.style.height = '100%';
-  modal.style.display = 'block';
-  modal.classList.remove('hidden');
-}
-
-function showNoVideoMessage(posterUrl) {
-  const noVideoMsg = document.querySelector('.no-video-msg');
-  noVideoMsg.innerHTML = `
-    <div>
-      <p>OOPS... We are very sorry! But we couldn't find the trailer.</p>
-    </div>
-    <div>
-      <img src="./images/noTrailer.png" alt="No Trailer">
-    </div>
-  `;
-}
-
-const closeBtn = document.querySelector('.close-btn__svg');
-
-closeBtn.addEventListener('click', closeTrailerModal);
-
-function closeTrailerModal() {
-  const trailerElement = document.querySelector('.trailer-element');
-  const noVideoMsg = document.querySelector('.no-video-msg');
-  const movieContainer = document.querySelector('.movie-container');
   const modal = document.querySelector('.trailer-modal');
+  const trailerElement = modal.querySelector('.trailer-element');
+  const closeButton = modal.querySelector('.close-btn__svg');
 
-  trailerElement.setAttribute('src', '');
-  noVideoMsg.textContent = '';
-  movieContainer.innerHTML = '';
-  modal.style.display = 'none';
+  async function loadTrailerById(movieId) {
+    try {
+      const movieTrailer = await getMovieTrailer(movieId);
+      const videos = movieTrailer.results;
+      if (videos.length > 0) {
+        const videoKey = videos[0].key;
+        const trailerUrl = `https://www.youtube.com/embed/${videoKey}?autoplay=1`;
+        showTrailerModal(trailerUrl);
+      } else {
+        showNoVideoMessage();
+      }
+    } catch (error) {
+      console.error('Request video error', error);
+    }
 
-  const closeButton = document.querySelector('.close-btn__svg');
-  closeButton.style.display = 'none';
-}
-
-window.addEventListener('click', function (event) {
-  const trailerModal = document.querySelector('.trailer-modal');
-
-  if (event.target === trailerModal) {
-    closeTrailerModal();
+    closeButton.style.display = 'block';
   }
+
+  function showTrailerModal(trailerUrl) {
+    trailerElement.setAttribute('src', trailerUrl);
+    modal.style.display = 'block';
+    modal.classList.remove('hidden');
+  }
+
+  function showNoVideoMessage() {
+    const noVideoMsg = modal.querySelector('.no-video-msg');
+    noVideoMsg.classList.remove('hidden');
+    noVideoMsg.classList.add('error-msg');
+  }
+
+  closeButton.addEventListener('click', closeTrailerModal);
+
+  function closeTrailerModal() {
+    trailerElement.setAttribute('src', '');
+    modal.style.display = 'none';
+    closeButton.style.display = 'none';
+    const noVideoMsg = modal.querySelector('.no-video-msg');
+    noVideoMsg.classList.add('hidden');
+    noVideoMsg.classList.remove('error-msg');
+  }
+
+  modal.addEventListener('click', event => {
+    if (event.target === modal) {
+      closeTrailerModal();
+    }
+  });
 });
