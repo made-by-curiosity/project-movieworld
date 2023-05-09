3ee9da2258dfa6c3b4a1c2104c8823512d90fff3
TurtlePower <agnkvdm@gmail.com>
2023-05-09 18:54:00 +0300

    fix movie modal info bug

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -1,11 +1,13 @@
-import { getFullMovieInfo } from "./fetchmoviedata";
-import { saveMovie } from "./local-storage-service"
+import { getFullMovieInfo } from './fetchmoviedata';
+import { saveMovie } from './local-storage-service';
 
-const openModalMovie = document.querySelector(".movie-gallery__list");
+const openModalMovie = document.querySelector('.movie-gallery__list');
 const backdrop = document.querySelector('.js-backdrop');
-const closeModalMovie = document.querySelector('[data-action="close-modal-movie"]');
-const movieGalleryEl = document.querySelector(".movie-gallery__list");
-const addToLibraryBtn = document.querySelector('.btn-lib .btn');
+const closeModalMovie = document.querySelector(
+  '[data-action="close-modal-movie"]'
+);
+const movieGalleryEl = document.querySelector('.movie-gallery__list');
+
 let movieId;
 
 movieGalleryEl.addEventListener('click', e => {
@@ -20,24 +22,28 @@ openModalMovie.addEventListener('click', onOpenModalMovie);
 closeModalMovie.addEventListener('click', onCloseModalMovie);
 backdrop.addEventListener('click', onCloseBackdropClick);
 
-function onOpenModalMovie () {
+function onOpenModalMovie() {
   window.addEventListener('keydown', onEscPress);
   document.body.classList.add('show-modal-movie');
 
   getFullMovieInfo(movieId)
     .then(fullMovieInfo => {
-      console.log("масив інформації по фільму", fullMovieInfo);
-      
+      console.log('масив інформації по фільму', fullMovieInfo);
+
       const original_title = fullMovieInfo.original_title;
       const vote_average = fullMovieInfo.vote_average;
       const vote_count = fullMovieInfo.vote_count;
       const popularity = fullMovieInfo.popularity;
-      const genres = fullMovieInfo.genres.map((genre) => genre.name).join(", ");
+      const genres = fullMovieInfo.genres.map(genre => genre.name).join(', ');
       const overview = fullMovieInfo.overview;
-      const IMAGE_SRC = fullMovieInfo.poster_path ? `https://image.tmdb.org/t/p/original${fullMovieInfo.poster_path}` : 'https://raw.githubusercontent.com/made-by-curiosity/project-movieworld/main/src/images/moviecoverholder.jpg';
+      const IMAGE_SRC = fullMovieInfo.poster_path
+        ? `https://image.tmdb.org/t/p/original${fullMovieInfo.poster_path}`
+        : 'https://raw.githubusercontent.com/made-by-curiosity/project-movieworld/main/src/images/moviecoverholder.jpg';
       const modalContainer = document.querySelector('.modal-movie.container');
-      
-      modalContainer.insertAdjacentHTML('beforeend', `
+
+      modalContainer.insertAdjacentHTML(
+        'beforeend',
+        `
         <div class="modal-movie__poster-wrap" data-modal="modal-movie">
             <img class="modal-movie__poster js-modal" src="${IMAGE_SRC}" alt="movie image">
         </div>
@@ -69,51 +75,57 @@ function onOpenModalMovie () {
                 </button>
             </div>
         </div>
-      `);
+      `
+      );
 
       const themeCheckbox = document.querySelector('.theme-switcher__input');
-    
-    if (themeCheckbox.checked) {
-      toggleTheme();
-    }
 
-    function toggleTheme() {
-      const elementsToChange = document.querySelectorAll('.js-modal');
-  
-      elementsToChange.forEach(element => {
-      element.classList.toggle('light-theme');
-      });
-    }
+      if (themeCheckbox.checked) {
+        toggleTheme();
+      }
 
-    addToLibraryBtn.addEventListener('click', onMovieBtnClick);
-    function onMovieBtnClick(event) {
-     const movieId = event.target.id;
-     saveMovie(movieId);
-    }
-    
+      function toggleTheme() {
+        const elementsToChange = document.querySelectorAll('.js-modal');
+
+        elementsToChange.forEach(element => {
+          element.classList.toggle('light-theme');
+        });
+      }
+
+      // const addToLibraryBtn = document.querySelector('.btn-lib .btn');
+
+      // console.log(addToLibraryBtn);
+      // addToLibraryBtn.addEventListener('click', onMovieBtnClick);
+
+      // function onMovieBtnClick(event) {
+      //   const movieId = event.target.id;
+      //   saveMovie(movieId);
+      // }
     })
     .catch(error => {
       console.error(error);
     });
-  }
+}
 
 function onCloseModalMovie() {
   window.removeEventListener('keydown', onEscPress);
   document.body.classList.remove('show-modal-movie');
   const modalContainer = document.querySelector('.modal-movie.container');
-  const modalContent = modalContainer.querySelectorAll('.modal-movie__poster-wrap, .modal-movie__info, .modal-movie__button-wrap');
+  const modalContent = modalContainer.querySelectorAll(
+    '.modal-movie__poster-wrap, .modal-movie__info, .modal-movie__button-wrap'
+  );
   modalContent.forEach(content => content.remove());
 }
 
 function onCloseBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-        onCloseModalMovie();
-    }
+  if (event.currentTarget === event.target) {
+    onCloseModalMovie();
+  }
 }
 
 function onEscPress(event) {
-    const ESC_KEY_CODE = "Escape";
-    if (event.code === ESC_KEY_CODE) {
-        onCloseModalMovie();
-    }
+  const ESC_KEY_CODE = 'Escape';
+  if (event.code === ESC_KEY_CODE) {
+    onCloseModalMovie();
+  }
 }
