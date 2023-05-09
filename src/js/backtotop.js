f27dab0538a82ef272ac099e5a3f2e60422733c8
Darina33 <115955389+Darina33@users.noreply.github.com>
2023-05-09 16:17:49 +0300

    scroll

--- src/js/backtotop.js
+++ src/js/backtotop.js
@@ -6,20 +6,14 @@
 //     window.scrollTo(0, currentScroll - currentScroll / 10);
 //   }
 // }
-
-window.onscroll = function () { scrollFunction() };
-const btn = document.getElementById("myBtn");
-
-btn.addEventListener("click", topFunction)
-
-function scrollFunction() {
+ export function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
     }
 }
-function topFunction() {
+export function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
