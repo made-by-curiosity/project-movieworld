82036a55d302e78e5196713286e6bf3f498026c5
34Maryna <aupairtickets@gmail.com>
2023-05-10 17:16:58 +0300

    catalog/pag/scss

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -12,25 +12,22 @@ export function createCatalogPagination(videos, query) {
     // below default value of options
     totalItems: `${videos.total_results}`,
     itemsPerPage: `${videos.results.length}`,
-    visiblePages: 5,
+    visiblePages: 3,
     page: 1,
     centerAlign: false,
     firstItemClassName: 'tui-first-child',
     lastItemClassName: 'tui-last-child',
     template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      page: '<a href="#" class="tui-page-btn js-theme">{{page}}</a>',
       currentPage:
-        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
+        '<strong class="tui-page-btn tui-is-selected js-theme">{{page}}</strong>',
+        currentPage: '<a href="#" class="tui-page-btn js-theme tui-is-selected">{{page}}</a>',
+        moveButton:
+        '<a href="#" class="tui-page-btn js-theme tui-next tui-prev hide-{{type}}"></a>',
       disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-page-btn js-theme tui-is-disabled tui-{{type}}"></span>',
+        moreButton:
+        '<a href="#" class="tui-page-btn js-theme tui-{{type}}-is-ellip">'+
         '<span class="tui-ico-ellip">...</span>' +
         '</a>',
     },
@@ -58,25 +55,22 @@ export function createWeeklyTrendsPagination(trendsMovies) {
     // below default value of options
     totalItems: `${trendsMovies.total_results}`,
     itemsPerPage: `${trendsMovies.results.length}`,
-    visiblePages: 5,
+    visiblePages: 3,
     page: 1,
     centerAlign: false,
     firstItemClassName: 'tui-first-child',
     lastItemClassName: 'tui-last-child',
     template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      page: '<a href="#" class="tui-page-btn js-theme tui-border">{{page}}</a>',
       currentPage:
-        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
+        '<strong class="tui-page-btn tui-is-selected js-theme">{{page}}</strong>',
+        currentPage: '<a href="#" class="tui-page-btn js-theme tui-is-selected">{{page}}</a>',
+        moveButton:
+        '<a href="#" class="tui-page-btn tui-next tui-prev hide-{{type}}"></a>',
       disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-is-disabled tui-{{type}}"></span>',
+        moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">'+
         '<span class="tui-ico-ellip">...</span>' +
         '</a>',
     },
