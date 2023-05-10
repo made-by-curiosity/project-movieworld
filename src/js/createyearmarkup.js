3f929a43ff8c31c038d581db2010311110e9eea7
VlasiukOleg <ikslprint@gmail.com>
2023-05-10 20:11:54 +0300

    add scroll style

--- src/js/createyearmarkup.js
+++ src/js/createyearmarkup.js
@@ -1,6 +1,9 @@
 import { refs } from './refs';
 
-const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
+const years = [
+  2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011,
+  2010, 2009, 2008, 2007, 2006, 2005,
+];
 
 export function createDropdownYearList() {
   const markupYears = years
