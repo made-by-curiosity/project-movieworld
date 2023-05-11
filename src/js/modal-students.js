4eb34e28d14eb83503546196c52a369453c0bbd0
TurtlePower <agnkvdm@gmail.com>
2023-05-11 23:28:50 +0300

    add modal students esc close

--- src/js/modal-students.js
+++ src/js/modal-students.js
@@ -1,24 +1,37 @@
 (() => {
-	const refs = {
-		teamModal: document.getElementById('modal-team'),
-		openTeamModal: document.querySelector('.js-open-modal-team'),
-		closeTeamModal: document.querySelector('.js-close-modal-team'),
-	}
-	
-	const onClickBtnOpen = () => {
-		refs.teamModal.classList.remove('js-hidden');
-  };
-	
-	const onClickBtnClose = event => {
-		if (event.target.classList[0] !== 'modal-team__icon-close' &&
-				event.target.classList[0] !== 'backdrop-modal') {
-      return;
-		}
-		
-		refs.teamModal.classList.add('js-hidden');
+  const refs = {
+    teamModal: document.getElementById('modal-team'),
+    openTeamModal: document.querySelector('.js-open-modal-team'),
+    closeTeamModal: document.querySelector('.js-close-modal-team'),
   };
 
-	refs.openTeamModal.addEventListener('click', onClickBtnOpen);
-	refs.closeTeamModal.addEventListener('click', onClickBtnClose);
-		refs.teamModal.addEventListener('click', onClickBtnClose);
+  const onClickBtnOpen = () => {
+    refs.teamModal.classList.remove('js-hidden');
+
+    window.addEventListener('keydown', onEscPress);
+    refs.closeTeamModal.addEventListener('click', onClickBtnClose);
+    refs.teamModal.addEventListener('click', onClickBtnClose);
+  };
+
+  const onClickBtnClose = event => {
+    if (
+      event.target.classList[0] !== 'modal-team__icon-close' &&
+      event.target.classList[0] !== 'backdrop-modal'
+    ) {
+      return;
+    }
+
+    refs.teamModal.classList.add('js-hidden');
+    refs.closeTeamModal.removeEventListener('click', onClickBtnClose);
+    refs.teamModal.removeEventListener('click', onClickBtnClose);
+  };
+
+  function onEscPress(e) {
+    if (e.code === 'Escape') {
+      refs.teamModal.classList.add('js-hidden');
+      window.removeEventListener('keydown', onEscPress);
+    }
+  }
+
+  refs.openTeamModal.addEventListener('click', onClickBtnOpen);
 })();
